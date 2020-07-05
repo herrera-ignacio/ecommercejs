191c621e5be08280e70efc4d65345b7303f2e3f8
Ignacio Roman Herrera <ignacioromanherrera@gmail.com>
2020-07-05 10:53:56 -0300

    API setup

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { graphqlHTTP } = require('express-graphql')
+const { buildSchema } = require('graphql')
+
+const app = express();
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+	res.send('Hello world')
+});
+
+app.use('/graphql', graphqlHTTP({
+	schema: buildSchema(`
+		type RootQuery {
+			events: [String!]!
+		}
+
+		type RootMutation {
+			createEvent(name: String): String
+		}
+
+		schema {
+			query: RootQuery
+			mutation: RootMutation
+		}
+	`),
+	rootValue: {
+		events: () => ['Romantic', 'Cooking', 'Sailing'],
+		createEvent: (args) => {
+			const eventName = args.name
+			return eventName
+		}
+	},
+	graphiql: true
+}))
+
+app.listen(3000, () => console.log('Listening on port 3000'));
